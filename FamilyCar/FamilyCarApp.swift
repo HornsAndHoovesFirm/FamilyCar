ceb82025297b2cc6e7d45a5390092637c23d3e92
Oleg Chernobelsky <chy.oleg.code@gmail.com>
2025-03-12 23:32:07 +0200

    Initial Commit

---
+++ FamilyCar/FamilyCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyCarApp.swift
+//  FamilyCar
+//
+//  Created by Oleg Chernobelsky on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FamilyCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
